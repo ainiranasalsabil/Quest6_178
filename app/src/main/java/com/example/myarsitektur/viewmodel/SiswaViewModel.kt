a538d6306d9cc717092751eae9a589a6c6a4aa15
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-21 10:28:22 +0700

    membuat set siswa untuk nama gender dan alamat

--- app/src/main/java/com/example/myarsitektur/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitektur/viewmodel/SiswaViewModel.kt
@@ -12,4 +12,8 @@ class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow(value = Siswa())
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 
+    fun setSiswa(ls: MutableList<String>){
+        _statusUI.update { statusSaatIni } ->
+        statuSaatIni.copy(nama=ls[0], gender=ls[1], alamat=ls[2])
+    }
 }
