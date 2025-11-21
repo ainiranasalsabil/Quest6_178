c81fd804e6da3d5340167c43fc05654a8b29e6a3
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-21 10:26:26 +0700

    membuat varialel privat status ui

--- app/src/main/java/com/example/myarsitektur/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitektur/viewmodel/SiswaViewModel.kt
@@ -1,4 +1,15 @@
 package com.example.myarsitektur.viewmodel
 
-class SiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.myarsitektur.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(value = Siswa())
+    
+
 }
