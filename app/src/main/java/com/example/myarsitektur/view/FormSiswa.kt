60e8cfa935741a2d6780158ad70ce0b8ff30ca2d
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-21 11:00:56 +0700

    mengalamatkan gender

--- app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
@@ -8,22 +8,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import com.example.navigasi.R
+import com.example.myarsitektur.R
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -43,7 +46,7 @@ fun FormSiwa(
     var txtNama by rememberSaveable { mutableStateOf(value = "") }
     var txtAlamat by remember { mutableStateOf(value = "") }
     var txtGender by remember { mutableStateOf(value = "") }
-    val listData: MutableList<String> = mutableStateOf(txtNama,txtGender,txtAlamat)
+    val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
     Scaffold(modifier = Modifier,
         topBar = {
             TopAppBar(
@@ -86,42 +89,42 @@ fun FormSiwa(
             )
 
             Row {
-                PilihanJk.forEach{
-                        item ->
-                    Row(verticalAlignment = Alignment.CenterVertically) {
+                pilihanJK.forEach { item ->
+                    Row(
+                        modifier = Modifier.selectable(
+                            selected = txtGender == item,
+                            onClick = { txtGender = item }
+                        ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
                         RadioButton(
                             selected = txtGender == item,
-                            onClick = { txtGender }
+                            onClick = { txtGender = item }
                         )
                         Text(text = item)
                     }
                 }
             }
+            Spacer(modifier = Modifier.height(10.dp))
 
-            HorizontalDivider(
-                modifier = Modifier
-                    .padding(all = 20.dp)
-                    .width(width = 250.dp),
-                thickness = 1.dp,
-                color = Color.Red
-            )
             OutlinedTextField(
-                value = "",
+                value = txtAlamat,
                 singleLine = true,
-                modifier = Modifier
-                    .width(width = 250.dp),
-                label = {Text(text = "Alamat")},
-                onValueChange = {
-                    txtAlamat
-                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.width(250.dp),
+                label = { Text(text = "Alamat Lengkap") },
+                onValueChange = {  = it }
             )
-            Spacer(modifier = Modifier.height(height = 30.dp))
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // BUTTON SUBMIT
             Button(
-                modifier = Modifier.fillMaxWidth(fraction = 1f)
-                    .padding(all = 25.dp),
-                onClick = onSubmitButtonClicked
+                modifier = Modifier.width(250.dp),
+                enabled = txtAlamat.isNotEmpty(),
+                onClick = { onSubmitButtonClicked(listData) }
             ) {
-                Text(text = stringResource(id = R.string.submit))
+                Text(stringResource(id = R.string.submit))
             }
         }
     }
