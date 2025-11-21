f864871705668fb949dcd28ac4041dfe16bacf9d
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-21 11:47:11 +0700

    menambahkan parameter view model

--- app/src/main/java/com/example/myarsitektur/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/myarsitektur/view/uicontroller/Navigasi.kt
@@ -1,2 +1,34 @@
 package com.example.myarsitektur.view.uicontroller
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.myarsitektur.view.FormSiswa
+import com.example.myarsitektur.view.TampilSiswa
+import com.example.myarsitektur.viewmodel.SiswaViewModel
+
+enum class Navigasi {
+    FormSiswa,
+    DetailSiswa
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    siswaViewModel: SiswaViewModel = viewModel()
+) {
+
+private fun backToForm(navController: NavHostController) {
+    navController.popBackStack(
+        route = Navigasi.FormSiswa.name,
+        inclusive = false
+    )
+}
